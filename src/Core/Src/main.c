35d8e66e8b9d80c42c42db9876f1e0f7795a93a6
Tomiboom333 <tomasriquelme1239@gmail.com>
2026-08-24 00:28:01 -0300

    Cambios en el adc y buffer

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -122,7 +122,7 @@ int main(void)
     for (int i = 0; i < 4; i++) {//asegurarse que lo demas es 0
         RxBuffer[i] = 0x00;
     }
-    HAL_SPI_Receive(&hspi1, (uint8_t*)RxBuffer, 4, HAL_MAX_DELAY); 
+    HAL_SPI_Receive(&hspi1, (uint8_t*)RxBuffer, 4, 100); 
     //if(HAL_SPI_TransmitReceive(&hspi1, (uint8_t*)TxBuffer, (uint8_t*)RxBuffer, 4, HAL_MAX_DELAY)==HAL_OK){
     //  spiRxReady=true;
     //if (RxBuffer[0] != 0x01)
@@ -139,22 +139,34 @@ int main(void)
     {
       estAct.modId[i] = HAL_GPIO_ReadPin(GPIOB, entradasD[i]);
     }
-     for (int i = 0; i < 4; i++) {//asegurarse que lo demas es 0
+     for (int i = 0; i < 3; i++) {//asegurarse que lo demas es 0
         TxBuffer[i] = 0x00;
     }
     for (int i = 0; i < 8; i++) {
       TxBuffer[0] |= (estAct.modId[i] ? 1 : 0) << i;
     }
-    HAL_ADC_Start(&hadc1);
-    estAct.modIa[0] = (uint8_t)(HAL_ADC_GetValue(&hadc1));
-    estAct.modIa[1] = (uint8_t)(HAL_ADC_GetValue(&hadc1));
-    HAL_ADC_Stop(&hadc1);
+    estAct.modIa[0] = 0;
+    estAct.modIa[1] = 0;
+
+    if (HAL_ADC_Start(&hadc1) == HAL_OK)
+    {
+      if (HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK)
+      {
+        estAct.modIa[0] = (uint8_t)HAL_ADC_GetValue(&hadc1);
+      }
+      if (HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK)
+      {
+        estAct.modIa[1] = (uint8_t)HAL_ADC_GetValue(&hadc1);
+      }
+      HAL_ADC_Stop(&hadc1);
+    }
+
     TxBuffer[1] = estAct.modIa[0];
     TxBuffer[2] = estAct.modIa[1];
 
     /* Queda listo para el siguiente comando, sin bloquear. */
     
-    if(HAL_SPI_Transmit(&hspi1, (uint8_t*)TxBuffer, 3, 1000) == HAL_OK) HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, 0);
+    if(HAL_SPI_Transmit(&hspi1, (uint8_t*)TxBuffer, 3, 100) == HAL_OK) HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, 0);
     else HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, 1);
     if(1 == 1);
     //else{
